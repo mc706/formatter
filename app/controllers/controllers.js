62ef6bbba10c4d27e10772de48d5027aa29bb211
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-17 19:55:30 -0400

    linting controllers

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,69 +1,71 @@
-app.controller('FormatController', function($scope){
-    
-    $scope.loadData = function(){
+app.controller('FormatController', function ($scope) {
+    "use strict";
+
+    $scope.loadData = function () {
         $scope.data = [];
-        var data = angular.copy($scope.raw);
-        var lines = data.split('\n');
+        var data = angular.copy($scope.raw),
+            lines = data.split('\n');
         $scope.headers = lines[0].split(',');
-        lines.splice(0,1);
-        angular.forEach(lines, function(line){
-            fields = line.split(',');
-            var temp = {};
-            for (var i = 0; i < $scope.headers.length; i++){
+        lines.splice(0, 1);
+        angular.forEach(lines, function (line) {
+            var fields = line.split(','),
+                temp = {},
+                i;
+            for (i = 0; i < $scope.headers.length; i += 1) {
                 temp[$scope.headers[i]] = fields[i];
             }
             $scope.data.push(temp);
         });
         $scope.checkCases();
     };
-    
+
     $scope.unmatched = angular.copy($scope.data);
-    
-    $scope.checkCases = function(){
+
+    $scope.checkCases = function () {
         $scope.unmatched = angular.copy($scope.data);
-        if ($scope.cases.length === 0){
+        if ($scope.cases.length === 0) {
             return false;
         }
-        angular.forEach($scope.cases, function(rcase){
+        angular.forEach($scope.cases, function (rcase) {
             rcase.matches = [];
             var remove = [];
-            if (rcase.rules.length === 0){
+            if (rcase.rules.length === 0) {
                 return false;
             }
-            angular.forEach($scope.unmatched, function(entry){
-               var matched = [];
-               angular.forEach(rcase.rules, function(rule){
-                   if (rule.value=='*'){
-                        if (entry[rule.field] !== ''){
-                            matched.push(true);
-                        } else {
-                            matched.push(false);
-                        }                   
-                   }else{
-                       var val = rule.value == '_' ? '':rule.value;
-                        if (entry[rule.field] == val){
+            angular.forEach($scope.unmatched, function (entry) {
+                var matched = [];
+                angular.forEach(rcase.rules, function (rule) {
+                    if (rule.value === '*') {
+                        if (entry[rule.field] !== '') {
                             matched.push(true);
                         } else {
                             matched.push(false);
                         }
-                   }
-               });
-               if (matched.every(Boolean)){
-                   rcase.matches.push(entry);
-                   remove.push(entry);
-               }
+                    } else {
+                        var val = rule.value === '_' ? '' : rule.value;
+                        if (entry[rule.field] === val) {
+                            matched.push(true);
+                        } else {
+                            matched.push(false);
+                        }
+                    }
+                });
+                if (matched.every(Boolean)) {
+                    rcase.matches.push(entry);
+                    remove.push(entry);
+                }
             });
-            angular.forEach(remove, function(toremove){
-                $scope.unmatched.splice($scope.unmatched.indexOf(toremove),1);
+            angular.forEach(remove, function (toremove) {
+                $scope.unmatched.splice($scope.unmatched.indexOf(toremove), 1);
             });
         });
         $scope.processOutput();
     };
 
-    $scope.getValues = function(rcase){
-        var vals = ['*','_'];
-        angular.forEach($scope.data, function(entry){
-            if (vals.indexOf(entry[rcase.field]) == -1){
+    $scope.getValues = function (rcase) {
+        var vals = ['*', '_'];
+        angular.forEach($scope.data, function (entry) {
+            if (vals.indexOf(entry[rcase.field]) === -1) {
                 vals.push(entry[rcase.field]);
             }
         });
@@ -72,61 +74,65 @@ app.controller('FormatController', function($scope){
 
     $scope.cases = [
     ];
-  
-    $scope.addCase = function(){
+
+    $scope.addCase = function () {
         $scope.cases.push({
-            rules:[],
-            output:'',
-            matches:[]
+            rules: [],
+            output: '',
+            matches: []
         });
     };
-    
-    $scope.removeCase = function(rcase){
-        $scope.cases.splice($scope.cases.indexOf(rcase),1);
+
+    $scope.removeCase = function (rcase) {
+        $scope.cases.splice($scope.cases.indexOf(rcase), 1);
         $scope.checkCases();
-    }
-    
-    $scope.cloneCase = function(rcase){
+    };
+
+    $scope.cloneCase = function (rcase) {
         $scope.cases.push(angular.copy(rcase));
         $scope.checkCases();
-    }
-    $scope.addRule = function(rcase){
-        rcase.rules.push({field:rcase.field, value:rcase.value});
+    };
+
+    $scope.addRule = function (rcase) {
+        rcase.rules.push({field: rcase.field, value: rcase.value});
         delete rcase.field;
         delete rcase.value;
         $scope.checkCases();
     };
-    
-    $scope.removeRule = function(rcase,rule){
-        rcase.rules.splice(rcase.rules.indexOf(rule),1);
+
+    $scope.removeRule = function (rcase, rule) {
+        rcase.rules.splice(rcase.rules.indexOf(rule), 1);
         $scope.checkCases();
         $scope.processOutput();
     };
-    
+
     $scope.output = "";
-    $scope.processOutput = function(){
+    $scope.processOutput = function () {
         $scope.output = '<?xml version="1.0" encoding="UTF-8"?>';
-        rx1 = /{{(.*)}}/g; //replace all {{}} variables
-        rx2 = /{\[[^\{\}\[\]]*(>\s?<\/)[^\{\}\[\]]*\]}/g; //Remove optional Lines
-        rx3 = /({\[|\]})/g; //clean up rest of tags
-        angular.forEach($scope.cases, function(rcase){
-            angular.forEach(rcase.matches, function(match){
+        var rx1 = /{{(.*)}}/g, //replace all {{}} variables
+            rx2 = /{\[[^\{\}\[\]]*(>\s?<\/)[^\{\}\[\]]*\]}/g, //Remove optional Lines
+            rx3 = /({\[|\]})/g, //clean up rest of tags
+            out,
+            results,
+            k;
+        angular.forEach($scope.cases, function (rcase) {
+            angular.forEach(rcase.matches, function (match) {
                 out = rcase.output;
-                while ((results = rx1.exec(out)) !== null){
-                    if (results){
-                        out = out.replace(results[0],match[results[1]]);
+                while ((results = rx1.exec(out)) !== null) {
+                    if (results) {
+                        out = out.replace(results[0], match[results[1]]);
                     }
                 }
-                for (var k=0;k<3;k++){ //loop 4 times because javascript cannot handle recurive regex
-                    while ((results = rx2.exec(out)) !== null){
-                        if (results){
-                            out = out.replace(results[0],'');
+                for (k = 0; k < 3; k += 1) { //loop 4 times because javascript cannot handle recurive regex
+                    while ((results = rx2.exec(out)) !== null) {
+                        if (results) {
+                            out = out.replace(results[0], '');
                         }
                     }
                 }
-                while ((results = rx3.exec(out)) !== null){
-                    if (results){
-                        out = out.replace(results[0],'');
+                while ((results = rx3.exec(out)) !== null) {
+                    if (results) {
+                        out = out.replace(results[0], '');
                     }
                 }
                 $scope.output += out;
@@ -134,25 +140,25 @@ app.controller('FormatController', function($scope){
         });
         $scope.output = vkbeautify.xml($scope.output, 4);
     };
-    
+
     $scope.custom = true;
-    $scope.toggleCustom = function(){
-        $scope.custom = $scope.custom === false ? true: false;
+    $scope.toggleCustom = function () {
+        $scope.custom = !$scope.custom;
     };
-    
+
     $scope.demo = true;
-    $scope.toggleDemo = function(){
-        $scope.demo = $scope.demo === false ? true: false;
+    $scope.toggleDemo = function () {
+        $scope.demo = !$scope.demo;
     };
-    
-    $scope.totalMatched = function(){
+
+    $scope.totalMatched = function () {
         var total = 0;
-        angular.forEach($scope.cases, function(rcase){
+        angular.forEach($scope.cases, function (rcase) {
             total += rcase.matches.length;
         });
         return total;
     };
-    
+
     $scope.demoOut = "<transaction>\n\
 <name>\n\
 <firstName>{{First Name}}</firstName>\n\
